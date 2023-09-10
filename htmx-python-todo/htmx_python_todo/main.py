de2d5eae2a06f3f2c2a771cd7064eceb2579bd96
josiah <josiah@appstack.nyc>
2023-09-10 13:19:28 -0400

    Implement templates for py project

--- htmx-python-todo/htmx_python_todo/main.py
+++ htmx-python-todo/htmx_python_todo/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def main():
-    return {"message": "Hey, It is me hey"}
+@app.get("/", response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse("index.html", dict(request=request))
