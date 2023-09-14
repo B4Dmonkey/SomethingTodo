e983f361a2e1aa084892bfd6bcb7e03e96605124
josiah <josiah@appstack.nyc>
2023-09-14 06:36:25 -0400

    Make model a data class

--- htmx-python-todo/htmx_python_todo/models.py
+++ htmx-python-todo/htmx_python_todo/models.py
@@ -1,8 +1,8 @@
-class TodoItem: # ? Its been a while since I've used python but a named tuple might be better ? 
-    def __init__(self, title: str, completed: bool = False,itemId:int=None):
-        self.title = title
-        self.completed = completed
-        self.itemId = itemId
+from dataclasses import dataclass, field
 
-    def __repr__(self):
-        return f"<TodoItem: {self.title} ({self.completed})>"
+
+@dataclass(frozen=True)
+class TodoItem:
+    title: str
+    completed: bool = field(default=False)
+    itemId: int = field(default=None)
