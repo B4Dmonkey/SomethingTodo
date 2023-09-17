d46c6bbed04ac3eedf5b4b025d4c7a5e5a6a7bcc
josiah <josiah@appstack.nyc>
2023-09-17 17:39:06 -0400

    Change the default response type to html

--- htmx-python-todo/htmx_python_todo/main.py
+++ htmx-python-todo/htmx_python_todo/main.py
@@ -6,19 +6,27 @@ from .crud import create, readAll, update, delRecord
 from .models import TodoItem
 
 
-app = FastAPI()
+app = FastAPI(default_response_class=HTMLResponse)
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def main(request: Request):
     todoItems = readAll()
     return templates.TemplateResponse("index.html", dict(request=request, todoItems=todoItems))
+
+
+@app.post("/")
+def temp(todo: Annotated[str, Form()]):
+    newTodo = TodoItem(title=todo)
+    # create(newTodo)
+    print(f"posted to the wrong route? new todo item: {newTodo}")
+    return "<h1>Good night!!!</h1>"
 
 
 @app.post("/create")
 def createItem(todo: Annotated[str, Form()]):
     newTodo = TodoItem(title=todo)
     create(newTodo)
-
+    print(f"created new todo item: {newTodo}")
     return "Happy Birthday!!!"
